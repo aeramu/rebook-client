4c108b3a3c2d5661d37160412ff73a0d7b1ab983
harris61 <harristioadam2@gmail.com>
2021-02-28 18:16:56 +0700

    add bookcard

---
+++ src/components/book/Card/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { TouchableHighlight, View, Text, StyleSheet, Image} from 'react-native'
+
+export default (props) => {
+    const {title, author, imageUrl} = props
+    return(
+        <View style={styles.container}>
+            <Image
+            resizeMode='stretch' 
+            style={styles.image} 
+            source={require('../../../../assets/kajian.png')}
+            />
+            <View style={styles.tag}>
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                <Text style={styles.subtitle}>
+                    {author}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'stretch',
+        justifyContent: 'flex-start', 
+        borderRadius: 10,  
+        overflow:'hidden',
+        borderWidth: 0.75,
+        borderColor: 'grey',
+    },
+    image: {
+        width: 150,  
+        height: 200,   
+    },
+    tag: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingTop: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    subtitle:   {
+        fontSize: 15,
+        color: 'grey',
+        justifyContent: 'center',
+    }
+  });
+
+

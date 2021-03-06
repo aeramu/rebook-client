7c31e43a3e133ec87957f29115fe3703e338cf3c
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2021-03-06 21:47:43 +0700

    show cards

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import {useQuery, gql} from '@apollo/client'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, Dimensions } from 'react-native';
+import { useQuery, gql } from '@apollo/client'
 
 import Header from '../../components/common/Header'
+import Card from '../../components/book/Card'
+
+const initWindowWidth = Dimensions.get("window").width;
 
 export default ({navigation}) => {
+  const [windowWidth, setWindowWidth] = useState( initWindowWidth );
+
+  const onChange = ({ window }) => {
+    setWindowWidth( window.width );
+  };
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  });
+
+  const numColumns = Math.floor(windowWidth / 250) 
+
   const { loading, data } = useQuery(GET_BOOK_LIST,{
     variables: {
       first: 50,
@@ -31,10 +49,24 @@ export default ({navigation}) => {
   const onButtonPress = () => {
     navigation.navigate('Book')
   }
-
+ 
   return (
     <View style={styles.container}>
       <Header/>
+      <FlatList
+        key={numColumns}
+        numColumns={numColumns ? numColumns : 1}
+        data={data.getBookList.edges}
+        renderItem={({item}) => <View style={styles.cardContainer}>
+          <Card
+            title={item.title}
+            author={item.author}
+            imageUrl={item.coverImage}
+            onPress={()=>alert(`Kamu mencet ${item.title}`)}
+          />
+        </View>}
+        keyExtractor={item => item.id }
+      />
       <Button
         title="Go to Book Page"
         onPress={onButtonPress}
@@ -50,6 +82,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  cardContainer : {
+    margin:20
+  }
 });
 
 const GET_BOOK_LIST = gql`

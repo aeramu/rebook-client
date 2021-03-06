88a510da5140de6ded09c8dfe692096fa51c3320
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2021-03-06 15:20:26 +0700

    delete gql

--- src/config/graphql.js
+++ src/config/graphql.js
@@ -1,6 +1,6 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: 'http://rebook-server.herokuapp.com/graphql',
   cache: new InMemoryCache()
-});
+})

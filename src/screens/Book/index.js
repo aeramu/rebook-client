32ed8ded9ba5dbc2f6104fe3c5012d8153624adf
harris61 <harristioadam2@gmail.com>
2021-03-06 17:35:43 +0700

    edit book detail summary component

--- src/screens/Book/index.js
+++ src/screens/Book/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import Summary from './Summary'
+
 export default () => {
     return (
         <View>
             <Text>This is Book Page</Text>
+            <Summary
+                style={{width:500}}
+            />
         </View>
     )
 }

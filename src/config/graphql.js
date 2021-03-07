166906d1f95ac7567f63cb3ba68f0436cc5ee748
aeramu <sulam3010@gmail.com>
2021-03-07 15:23:53 +0700

    add gh-pages deployment

--- src/config/graphql.js
+++ src/config/graphql.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://rebook-server.herokuapp.com/graphql',
+  uri: 'https://rebook-server.herokuapp.com/graphql',
   cache: new InMemoryCache()
 })
